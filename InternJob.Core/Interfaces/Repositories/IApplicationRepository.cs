50a8b3c67fc18bcdc4ae27e8f3f075e3b6119b1e
sawn05 <sanglaida@gmail.com>
2026-07-02 10:15:00 +0700

    feat(job): interfaces repository

---
+++ InternJob.Core/Interfaces/Repositories/IApplicationRepository.cs
@@ -0,0 +1,11 @@
+﻿using InternJob.Core.Entities;
+
+namespace InternJob.Core.Interfaces.Repositories;
+
+public interface IApplicationRepository
+{
+    Task<bool> ExistsAsync(int jobId, int candidateId);
+    Task<List<Application>> GetByCandidateIdAsync(int candidateId);
+    Task AddAsync(Application application);
+    Task SaveChangesAsync();
+}
---
+++ InternJob.Core/Interfaces/Repositories/ICVRepository.cs
@@ -0,0 +1,11 @@
+﻿using InternJob.Core.Entities;
+
+namespace InternJob.Core.Interfaces.Repositories;
+
+public interface ICVRepository
+{
+    Task<CV?> GetByIdAsync(int cvId);
+    Task<List<CV>> GetByCandidateIdAsync(int candidateId);
+    Task AddAsync(CV cv);
+    Task SaveChangesAsync();
+}
